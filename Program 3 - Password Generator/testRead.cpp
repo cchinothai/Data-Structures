b152ae91ae35ea72e81a65fb0acf94c50a31b843
cchinothai <81498302+cchinothai@users.noreply.github.com>
2022-05-08 19:11:34 -0700

    Add files via upload

---
+++ Program 3 - Password Generator/testRead.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Cody Chinothai on 11/5/21.
+//
+
+#include "testRead.h"
---
+++ Program 3 - Password Generator/testRead.h
@@ -0,0 +1,14 @@
+//
+// Created by Cody Chinothai on 11/5/21.
+//
+
+#ifndef PROGRAM3_TESTREAD_H
+#define PROGRAM3_TESTREAD_H
+
+
+class testRead {
+
+};
+
+
+#endif //PROGRAM3_TESTREAD_H
